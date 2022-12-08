ead536826b6e6fce201335b81c3f0cdcb59d5ec7
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-12-08 23:35:33 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show, :index]
+  resources :stores, only: [:create, :index,:show, :update, :destroy]
+  resources :requested_items
+  resources :items
+
+ # merchant/user sign in routes  
+  post "/signup",  to: "users#create"
+  post '/auth/login', to: 'sessions#user_login'
+  delete '/auth/logout', to: 'sessions#user_logout'
+  # clerk sign in routes
+  resources :clerks, only: [:index, :show, :create, :update, :destroy ]
+  post '/clerk/login', to: 'sessions#clerk_login'
+  delete 'clerk/logout', to: 'sessions#clerk_logout'
+   post'/stores', to: "stores#create"
+   get '', to:"stores#index"
+ # admin sign in routes  
+  get"/stores/:id/items", to: "stores#store_items"
+   #post"/items", to: "items#create"
+  #delete"/items/:id", to: "items#destroy"
+  patch '/items/:id',to:"items#update"
+  resources :admins, only: [:create, :index, :show,:update, :destroy]
+  post '/admin/login', to: 'sessions#admin_login'
+  delete 'admin/logout', to: 'sessions#admin_logout'
+  
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
